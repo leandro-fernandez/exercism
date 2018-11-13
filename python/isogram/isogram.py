b1944e42daedc9f007807a595bc70db03de0fb14
Fernandez <af.leandro@gmail.com>
2018-11-13 13:36:48 -0200

    second iteration of isogram, using a set

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,10 +1,3 @@
 def is_isogram(string):
-    lowerString = string.lower()
-    dict = {}
-
-    for char in lowerString:
-      if char.isalpha():
-        if (char in dict): return False
-        else: dict[char] = 1
-    
-    return True
+    lowerString = string.lower().replace(" ", "").replace("-", "")
+    return len(set(lowerString)) == len(lowerString)
