56bb86dc96838f107225d80ac32f71575bd3242a
Fernandez <af.leandro@gmail.com>
2018-11-14 10:33:38 -0200

    second solution to rna transcritpion using str.maketrans

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,9 +1,3 @@
 def to_rna(dna_strand):
-  gene_map = {
-    "G": "C",
-    "C": "G",
-    "T": "A",
-    "A": "U"
-  }
-
-  return "".join([gene_map[gene] for gene in dna_strand])
+  trans = str.maketrans("GCTA", "CGAU")
+  return dna_strand.translate(trans)
